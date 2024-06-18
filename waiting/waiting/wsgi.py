31adc3f3303cb2b22a984caad4750bd65e219b32
JUNGUIHEON <jungui4366@gmail.com>
2024-06-18 10:55:43 +0900

    feat: [정의헌] 프로젝트 clone하기

--- waiting/waiting/wsgi.py
+++ waiting/waiting/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for waiting project.
+WSGI config for HZ_proj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "waiting.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HZ_proj.settings')
 
 application = get_wsgi_application()
