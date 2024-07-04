6a582d47c7ec84131dd769394ff9accbed3fff94
Inheon Choi <cih956964@gmail.com>
2024-07-04 18:57:41 +0900

    test: [최인헌] PR test [HZD]

--- waiting/waiting/settings.py
+++ waiting/waiting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q4_7x*-lpbkl5u#+9oho7b%pfj!r!i_y%_f5alj6$p2)cbc38p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.55', 'localhost', '192.168.0.45']
+ALLOWED_HOSTS = ['192.168.0.55', 'localhost', '127.0.0.1']
 
 # Application definition
 
